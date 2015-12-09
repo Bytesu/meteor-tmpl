abae99ddccef54dbd557f05ef93b511487d9b6ad
苏伟明 <byte_su@163.com>
2015-12-09 16:23:25 +0800

    meteor after response

--- server/server-router.js
+++ server/server-router.js
@@ -2,20 +2,41 @@
  * Created by s_ on 15/11/22.
  */
 
+Router.after(function(req,res){
+    // Iron.Router.bodyParser
+    // console.log(req.params)
+    // console.log((this.request.body))
+    console.log('after ')
+    this.response.write('tteessss');
+    this.response.end();
+    // next();
+},{where:'server'});
+
 Router.before(function(req,res,next){
     // Iron.Router.bodyParser
     // console.log(req.params)
-    console.log((this.request.body))
-    console.log('before ')
+    // console.log((this.request.body))
+    console.log('before ');
+    var ended = false;
+    var write  =  this.response.write;
+    this.response.write = function(chunk,encoding){
+        // try{
+        //     chunk = JSON.parse(chunk);
+        // }catch(e){
+        //     chunk = chunk;
+        // }
+        // chunk.name = "----------";
+       return write(chunk,encoding);
+   }
     next();
 },{where:'server'})
-Router.onBeforeAction(function(req,res,next){
-    // Iron.Router.bodyParser
-    // console.log(req.params)
-    console.log('beforeAction ')
-    console.log((this.request.body))
-    next();
-},{where:'server'})
+// Router.onBeforeAction(function(req,res,next){
+//     // Iron.Router.bodyParser
+//     // console.log(req.params)
+//     console.log('beforeAction ')
+//     console.log((this.request.body))
+//     next();
+// },{where:'server'})
 // Router.use('/*', { where: 'server' ,action:function(){
 //     console.log(this.request.body)
 //     console.log('rout ')
@@ -33,9 +54,11 @@ Router.route('/posts/:id', { where: 'server' })
         // curl -XPOST --data "hong=2312" "http://localhost:3000/posts/xxxxxxx"
         var pos = Posts.find();
         //console.log(this.request.body)
-        this.response.write(JSON.stringify(pos.fetch()))
+        this.response.write(pos.fetch().length+'====')
+        this.response.write(('------'))
 
         this.response.end();
+        
 
     })
     .put(function () {
